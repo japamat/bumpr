68f862cc0151d60ff048b00c276a861cc624683f
Jason Matthias <jpmatthias90@gmail.com>
2019-08-12 12:46:29 -0700

    Add homepage saga and reducer to root

--- frontend/src/reducers.js
+++ frontend/src/reducers.js
@@ -3,12 +3,14 @@ import { connectRouter } from 'connected-react-router';
 
 import history from './utils/history';
 import appReducer from './Containers/App/reducer';
+import homePageReducer from './Containers/HomePage/reducer';
 
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
 const rootReducer = combineReducers({
-  app: appReducer,
+  global: appReducer,
+  home: homePageReducer,
   router: connectRouter(history),
 });
 
--- frontend/src/rootSaga.js
+++ frontend/src/rootSaga.js
@@ -1,8 +1,10 @@
 import { call, put, takeLatest, all } from 'redux-saga/effects';
 import { loginSagas } from './Containers/LoginPage/saga';
+import { homeSagas } from './Containers/HomePage/saga';
 
 export default function* rootSaga() {
   yield all([
     ...loginSagas,
+    ...homeSagas,
   ])
 }
