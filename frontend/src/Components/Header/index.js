2f144a7b876707aeff616447854f998508559939
Jason Matthias <jpmatthias90@gmail.com>
2019-08-13 15:50:51 -0700

    Remove unused links & images

--- frontend/src/Components/Header/index.js
+++ frontend/src/Components/Header/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
-import A from '../A';
-import Img from '../Img';
 import NavBar from './NavBar';
 import HeaderLink from './HeaderLink';
-import Banner from './banner.jpg';
 
 function Header() {
   return (
     <div>
       <NavBar>
-        <HeaderLink to="/">
-          Nav text root
-        </HeaderLink>
-        <HeaderLink to="/features">
-          nav text features
-        </HeaderLink>
-        <HeaderLink to="/slogans">
-          nav text slogans
+        <HeaderLink to="/home">
+          home
         </HeaderLink>
       </NavBar>
     </div>
