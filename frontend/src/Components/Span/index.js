e67012d363b5b279150621c6ed599eb472667de9
Jason Matthias <jpmatthias90@gmail.com>
2019-08-15 23:35:33 -0700

    Add props to span styled component

--- frontend/src/Components/Span/index.js
+++ frontend/src/Components/Span/index.js
@@ -4,6 +4,7 @@ const Wrapper = styled.span`
   padding: 0 0.5em;
   font-size: ${props => (props.fontSize ? props.fontSize : '1em')};
   font-weight: ${props => (props.fontWeight ? props.fontWeight : 'normal')};
+  color: ${props => (props.color ? props.color : props.theme.color)};
 `;
 
 export default Wrapper;
